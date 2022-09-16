e24ba8e674b84f041fb89a8b3ff3f356618f4b7c
danielanthonyl <danskyjs@icloud.com>
2022-09-16 11:55:48 -0300

    hypotenuse exercise

---
+++ findHypotenuse/answer.ts
@@ -0,0 +1,6 @@
+const sideA: number = 10;
+const sideB: number = 6;
+
+const hypotenuse: number = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+
+console.log(hypotenuse);
---
+++ findHypotenuse/index.ts
@@ -0,0 +1,4 @@
+/**
+ * So this is the Pythagorean theorem formula: hypotenuse = √a²+b²
+ * use number, its properties and methods get the hypotenuse based off this formula.
+ */
